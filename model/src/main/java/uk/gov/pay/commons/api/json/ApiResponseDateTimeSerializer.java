f1d141d4acb1a2ef5dbd2fe2aa7ea6335381398d
george racu <george.racu@digital.cabinet-office.gov.uk>
2019-01-29 15:21:42 +0000

    PP-4597 Added JSON serializer for ZonedDateTime
    - To keep our ZDT external representation consistent, we created a serializer
    that will format ZDT with millisecond precision. This will ease the
    transition to Java 11

---
+++ model/src/main/java/uk/gov/pay/commons/api/json/ApiResponseDateTimeSerializer.java
@@ -0,0 +1,29 @@
+package uk.gov.pay.commons.api.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+import java.time.ZonedDateTime;
+
+import static uk.gov.pay.commons.model.ApiResponseDateTimeFormatter.ISO_INSTANT_MILLISECOND_PRECISION;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ApiResponseDateTimeSerializer extends StdSerializer<ZonedDateTime> {
+
+    public ApiResponseDateTimeSerializer() {
+        this(null);
+    }
+
+    private ApiResponseDateTimeSerializer(Class<ZonedDateTime> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(ZonedDateTime value, JsonGenerator gen, SerializerProvider sp) throws IOException {
+        gen.writeString(ISO_INSTANT_MILLISECOND_PRECISION.format(value));
+    }
+}
