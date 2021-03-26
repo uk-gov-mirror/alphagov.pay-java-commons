ce744ea75c73d0cbd413b0be8402d94085de88e2
Oswald Quek <oswald.quek@digital.cabinet-office.gov.uk>
2021-03-26 10:47:07 +0000

    PP-7938: Fix maven javadoc plugin validation

--- validation/src/main/java/uk/gov/service/payments/commons/validation/DateTimeUtils.java
+++ validation/src/main/java/uk/gov/service/payments/commons/validation/DateTimeUtils.java
@@ -15,15 +15,14 @@ public class DateTimeUtils {
 
     /**
      * Converts any valid ZonedDateTime String (ISO_8601) representation to a UTC ZonedDateTime
-     * <p>
-     * e.g. <br/>
-     * 1. 2010-01-01T12:00:00+01:00[Europe/Paris] ==>  ZonedDateTime("2010-12-31T22:59:59.132Z") <br/>
-     * 2. 2010-12-31T22:59:59.132Z ==>  ZonedDateTime("2010-12-31T22:59:59.132Z") <br/>
-     * </p>
+     *
+     * e.g. 
+     * 1. 2010-01-01T12:00:00+01:00[Europe/Paris] to ZonedDateTime("2010-12-31T22:59:59.132Z")
+     * 2. 2010-12-31T22:59:59.132Z to ZonedDateTime("2010-12-31T22:59:59.132Z")
      *
      * @param dateString e.g.
-     * @return @ZonedDateTime instance represented by dateString in UTC ("Z") or
-     * @Optional.empty() if the dateString does not represent a valid ISO_8601 zone string
+     * @return ZonedDateTime instance represented by dateString in UTC ("Z") or
+     * Optional.empty() if the dateString does not represent a valid ISO_8601 zone string
      * @see "https://docs.oracle.com/javase/8/docs/api/java/time/ZonedDateTime.html"
      */
     public static Optional<ZonedDateTime> toUTCZonedDateTime(String dateString) {
@@ -39,11 +38,11 @@ public class DateTimeUtils {
 
     /**
      * Converts a LocalDateTime to a UTC ISO_8601 string representation
-     * <p>
-     * e.g. <br/>
-     * 1. LocalDateTime("2010-01-01") ==> "2010-12-01" <br/>
-     * 2. LocalDateTime("2010-12-31") ==> "2010-12-31" <br/>
-     * </p>
+     *
+     * e.g.
+     * 1. LocalDateTime("2010-01-01") to "2010-12-01"
+     * 2. LocalDateTime("2010-12-31") to "2010-12-31"
+     *
      *
      * @param zonedDateTime
      * @return UTC ISO_8601 date string
